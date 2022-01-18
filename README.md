5671ac06127caf4f5298e71dc09bc6e93cae2d6c
menelaia <mmenelaia@gmail.com>
2022-01-18 19:51:06 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# eslint-config-react
+# 🗂️ eslint-config-react
+[![NPM](https://img.shields.io/npm/v/@menelaia/eslint-config-react?style=flat-square)](https://www.npmjs.com/package/@menelaia/eslint-config-react) [![NPM License](https://img.shields.io/npm/l/@menelaia/eslint-config-react?style=flat-square)](https://opensource.org/licenses/MIT)
+
+Eslint config for React projects. Contains recommended rules extended from `airbnb`, `airbnb/hooks`, `plugin:react`, `plugin:react-hooks`. Added rules contain import and prop sorting.
+
+
+## 🖥️ Installation
+1. Install the correct versions of each package using the below command. If using older npm versions, you can install all needed dependencies manually.
+```sh
+npx install-peerdeps --dev @menelaia/eslint-config-react
+```
+2. Add `"extends": "@menelaia/react"` to your .eslintrc.
---
+++ index.js
@@ -0,0 +1,84 @@
+module.exports = {
+  extends: [
+    "airbnb",
+    "airbnb/hooks",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+  ],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module",
+    jsx: true,
+  },
+  plugins: [
+    "react",
+    "import",
+    "simple-import-sort",
+    "unused-imports"
+  ],
+  rules: {
+    "max-len": ["error", { "code": 80 }],
+    "no-console": "warn",
+    "no-nested-ternary": "off",
+    "no-use-before-define": 0,
+    "no-restricted-globals": 0,
+    "prefer-const": ["warn", {
+      "ignoreReadBeforeAssign": true
+    }],
+    "import/order": "off",
+    "import/extensions": 0,
+    "import/no-extraneous-dependencies": 0,
+    "import/prefer-default-export": 0,
+    "simple-import-sort/exports": "error",
+    "simple-import-sort/imports": [
+      "error",
+      {
+        "groups": [
+          [
+            // Packages. `react` related packages come first.
+            "^react",
+            // Side effect imports.
+            "^@?\\w",
+            "^\\u0000",
+            // Parent imports. Put `..` last.
+            "^\\.\\.(?!/?$)",
+            "^\\.\\./?$",
+            "^",
+            // Other relative imports. Put same-folder imports and `.` last.
+            "^\\./(?=.*/)(?!/?$)",
+            "^\\.(?!/?$)",
+            "^\\./?$"
+          ]
+        ]
+      }
+    ],
+    "unused-imports/no-unused-imports": 1,
+
+    // React rules
+    "react/no-did-update-set-state": "off",
+    "react/require-default-props": 2,
+    "react/forbid-prop-types": 0,
+    "react/static-property-placement": 0,
+    "react/display-name": "off",
+    "react/sort-comp": 0,
+    "react/jsx-filename-extension": [0, { "extensions": [".jsx", ".js"] }],
+    "react/jsx-props-no-spreading": 0,
+    "react/jsx-one-expression-per-line": 0,
+    "react/jsx-no-bind": 0,
+    "react/jsx-sort-props": [2, {
+      "callbacksLast": true,
+      "shorthandFirst": true,
+      "shorthandLast": false,
+      "ignoreCase": true,
+      "noSortAlphabetically": false,
+      "reservedFirst": ["key"]
+    }],
+    "react/sort-prop-types": [1, {
+      "callbacksLast": true,
+      "ignoreCase": true,
+      "requiredFirst": true,
+      "sortShapeProp": true,
+      "noSortAlphabetically": false
+    }]
+  },
+};
